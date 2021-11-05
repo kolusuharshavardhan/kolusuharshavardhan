ef54df19011180214216f92323e0546fe01e3ae5
kolusuharshavardhan <93757298+kolusuharshavardhan@users.noreply.github.com>
2021-11-05 06:17:11 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <title>login form</title>
+  </head> 
+ <body>
+ <div class="container"> 
+          <form class="login-username"> 
+              <p> class="login-text style="font-size: 2rem; font-weight: 800;">login</p>
+              <div class="input-group"> 
+                  <input type="username" placeholder="username" required>
+              </div>
+              <div class="input-group"> 
+                  <input type="password" placeholder="password" required>
+              </div>
+              <div class="input-group"> 
+                  <button> class="btn">login</button>
+              </div>
+              <p class="login-register-text"> new user? <a href="register.php">register</a>
+            </form>
+ </body> 
+</html>
